6bb177a72d1108e6ab21a0ede85f037cd51995f3
Souradeep Saha <46139736+SouradeepSaha@users.noreply.github.com>
2022-01-04 22:18:34 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 # Vim setup
-
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
@@ -14,3 +13,9 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # Install solarized theme
 cd ~/.vim/bundle
 git clone git://github.com/altercation/vim-colors-solarized.git
+
+
+# Iterm2 Setup
+
+# Install Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
