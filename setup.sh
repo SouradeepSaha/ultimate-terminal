e084ba763f27fecfc02ab05672e547b4b2792c0c
Souradeep Saha <46139736+SouradeepSaha@users.noreply.github.com>
2022-01-04 21:52:19 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,15 @@
 
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# Install Vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install solarized theme
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
